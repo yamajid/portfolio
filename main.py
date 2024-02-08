c6e2ce229df00fca4991334cc0d0290d7cf80406
Younes Amajid <yamajid@e3r5p4.1337.m>
2024-02-08 18:09:40 +0100

    last version [maybe it's done hh]

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    bk_image = url_for('static', filename="1.jpg")
+    return render_template("mainPage.html", bk_image=bk_image)
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
